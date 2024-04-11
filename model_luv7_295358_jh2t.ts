993138cfbd83e5b477b703e0b49e196583f70076
SipkoLarisa <larisasipko415@gmail.com>
2024-04-11 15:20:33 +0300

    Changed log output

---
+++ model_luv7_295358_jh2t.ts
@@ -0,0 +1,15 @@
+kiwi
+
+const getRandomSubset = (array, size) => array.slice(0, size);
+
+class MyClass { constructor() { this.property = getRandomString(); } }
+const getRandomSubset = (array, size) => array.slice(0, size);
+false - 17,41,47,41,50,66,57,41,93,19,35,46,99,18,18,53,90,32
+
+const removeDuplicates = array => Array.from(new Set(array));
+16 - 16
+
+const debounce = (func, delay) => { let timeout; return (...args) => { clearTimeout(timeout); timeout = setTimeout(() => func(...args), delay); }; };
+const removeDuplicates = array => Array.from(new Set(array));
+const timeoutPromise = (ms, value) => new Promise(resolve => setTimeout(() => resolve(value), ms));
+const isEven = num => num % 2 === 0;
